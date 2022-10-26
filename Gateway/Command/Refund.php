ab0f8895505a43d52c495d4b9e17382fc5e6a883
Gonzalo <gonzalo.martinez@improntus.com>
2022-10-26 17:47:09 -0300

    REB-22: [ID-30-31] Crear reembolsos.

--- Gateway/Command/Refund.php
+++ Gateway/Command/Refund.php
@@ -2,19 +2,73 @@
 
 namespace Improntus\Rebill\Gateway\Command;
 
+use Exception;
+use Improntus\Rebill\Model\Rebill\Payment as RebillPayment;
+use Improntus\Rebill\Model\Entity\Payment\Repository as PaymentRepository;
 use Magento\Payment\Gateway\CommandInterface;
 use Magento\Payment\Gateway\Command\ResultInterface;
 
 class Refund implements CommandInterface
 {
     /**
+     * @var RebillPayment $rebillPayment
+     */
+    protected RebillPayment $rebillPayment;
+
+    /**
+     * @var PaymentRepository $paymentRepository
+     */
+    protected PaymentRepository $paymentRepository;
+
+    public function __construct(
+        RebillPayment $rebillPayment,
+        PaymentRepository $paymentRepository
+    )
+    {
+        $this->rebillPayment = $rebillPayment;
+        $this->paymentRepository = $paymentRepository;
+    }
+
+    /**
      * @param array $commandSubject
      * @return ResultInterface|void|null
+     * @throws \Exception
      */
     public function execute(array $commandSubject)
     {
         /**
-         * @todo execute refund of the payment
+         * La estructura del dato de entrada la conozco a partir de
+         * \Magento\Payment\Model\Method\Adapter::refund
          */
+        $commandArgPayment = $commandSubject['payment'];
+        $order = $commandArgPayment->getOrder();
+
+        $items = $this->paymentRepository->getEzList([
+            'order_id' => ['eq' => $order->getId()],
+            'status' => ['eq' => 'SUCCEEDED']
+        ])->getItems();
+
+        $throwException = false;
+        foreach ($items as $payment) {
+
+            $response = $this->rebillPayment->refundPaymentById(
+                $payment->getData('rebill_id')
+            );
+
+            if (isset($response['statusCode']) || ( ! isset($response['status']))) {
+                /** Si es OK, la respuesta no trae 'statusCode', y si trae 'status' (el nuevo estado del pago: "REFUNDED"). */
+                $throwException = true;
+                continue;
+            }
+
+            /** Luego de hacer el reembolso actualizo el pago en Magento */
+            $payment->setStatus($response['status']);
+            $this->paymentRepository->save($payment);
+        }
+
+        if ($throwException) {
+            /** La razón de arrojar una excepción es que no se termine de realizar el reembolos en Magento. */
+            throw new Exception(__('Unable to refund payment on Rebill.'));
+        }
     }
 }
