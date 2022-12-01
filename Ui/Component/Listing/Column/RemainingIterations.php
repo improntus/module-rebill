b6005f1d5fbfccec89a748b721afae957ae6f611
Mauro Gonzalez <mauro.gonzalez@improntus.com>
2022-12-01 14:16:14 -0300

    Cuando entran a un producto que este marcado como "subscription only" y no tenga frecuencia se saca el boton para agregar al carrito. Se agrega en la vista del usuario y del admin la columna de iteraciones restantes en las suscripciones.

---
+++ Ui/Component/Listing/Column/RemainingIterations.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author Improntus Dev Team
+ * @copyright Copyright (c) 2022 Improntus (http://www.improntus.com/)
+ * @package Improntus_Rebill
+ */
+
+namespace Improntus\Rebill\Ui\Component\Listing\Column;
+
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+
+class RemainingIterations extends Column
+{
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface   $context,
+        UiComponentFactory $uiComponentFactory,
+        array              $components = [],
+        array              $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        foreach ($dataSource['data']['items'] as &$item) {
+            $name = $this->getData('name');
+            if ($item[$name] == "null") {
+                $item[$name] = "";
+            }
+        }
+
+        return $dataSource;
+    }
+}
